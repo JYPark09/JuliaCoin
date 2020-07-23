a765cef92e5e556f4399cb46b383ef17549a4118
Junyeong Park <jyp10987@gmail.com>
2020-07-23 23:43:13 +0900

    fix: Fix bugs

--- src/FieldElement.jl
+++ src/FieldElement.jl
@@ -1,6 +1,6 @@
 using Printf
 
-import Base: +, -, *, /, ^, ==
+import Base: +, -, *, /, ^, ==, !=
 
 struct FieldElement
     num::Int128
@@ -21,36 +21,40 @@ function ==(lhs::FieldElement, rhs::FieldElement)
     lhs.num == rhs.num && lhs.prime == rhs.prime
 end
 
+function ==(lhs::FieldElement, rhs::FieldElement)
+    !(lhs == rhs)
+end
+
 function +(lhs::FieldElement, rhs::FieldElement)
     @assert(lhs.prime == rhs.prime, "Cannot add two numbers in different fields.")
 
-    num = (lhs.num + rhs.num) % lhs.prime
+    num = mod1(lhs.num + rhs.num, lhs.prime)
     FieldElement(num, lhs.prime)
 end
 
 function -(lhs::FieldElement, rhs::FieldElement)
     @assert(lhs.prime == rhs.prime, "Cannot subtract two numbers in different fields.") 
 
-    num = (lhs.num - rhs.num) % lhs.prime
+    num = mod1(lhs.num - rhs.num, lhs.prime)
     FieldElement(num, lhs.prime)
 end
 
 function *(lhs::FieldElement, rhs::FieldElement)
     @assert(lhs.prime == rhs.prime, "Cannot multiply two numbers in different fields.")
 
-    num = (lhs.num * rhs.num) % lhs.prime
+    num = mod1(lhs.num * rhs.num, lhs.prime)
     FieldElement(num, lhs.prime)
 end
 
 function /(lhs::FieldElement, rhs::FieldElement)
     @assert(lhs.prime == rhs.prime, "Cannot divide two numbers in different fields.")
 
-    num = (lhs.num * ((rhs.num ^ (lhs.prime -2)) % lhs.prime)) % lhs.prime
+    num = mod1(lhs.num * mod1(rhs.num ^ (lhs.prime -2), lhs.prime), lhs.prime)
     FieldElement(num, lhs.prime)
 end
 
 function ^(lhs::FieldElement, exp::Int128)
     n = mod1(exp, lhs.prime - 1)
-    num = (lhs.num ^ n) % lhs.prime
+    num = mod1(lhs.num ^ n, lhs.prime)
     FieldElement(num, lhs.prime)
 end
